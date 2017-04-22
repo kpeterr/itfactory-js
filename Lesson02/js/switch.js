c83d62b8b001073f2f8c4b7fa073d359e93b657a
kpeterr <kpeterr2@gmail.com>
2017-04-22 11:01:19 +0200

    A switch.js létrehozása

---
+++ Lesson02/js/switch.js
@@ -0,0 +1,16 @@
+var number = 1;
+
+switch (number) {
+    case 0:
+        console.log('Zero');
+        break;
+    case 1:
+        console.log('One');
+        break;
+    case 2:
+        console.log('Two');
+        break;
+    default:
+        console.log('Many');
+        break;
+}
